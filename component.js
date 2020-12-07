8209a80f9dbc8efc3d04b9fb4fe0fd03d5aaffe9
Rebel <curiousdonkey17@gmail.com>
2020-12-07 05:44:20 +0530

    Add list component

--- component.js
+++ component.js
@@ -1,10 +1,78 @@
-Vue.component('button-counter', {
+// {
+//     "find" : "foo"
+//     replace: "bar"
+// }
+
+
+Vue.component('variable', {
+    props: ['findProp', 'replaceProp'],
     data: function () {
         return {
-            count: 0
+            find: this.findProp,
+            replace: this.replaceProp
         }
     },
-    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+    template: `<div class="card">
+  <div class="card-body bg-light">
+        <form class="form-inline">
+  <label class="sr-only" for="inlineFormInputName2">Variable</label>
+  <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="{NAME}" v-model="find">
+    &nbsp; in the template will be replaced with &nbsp;
+  <label class="sr-only" for="inlineFormInputGroupUsername2">Value</label>
+  <div class="input-group mb-2 mr-sm-2">
+    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="ex: Jhon"  v-model="replace">
+  </div>
+</form>
+  </div>
+</div>`
 })
 
-new Vue({ el: '#variables' })
+
+Vue.component('variable-list', {
+
+    template: `
+
+        <template v-for="variable in variableList">
+            
+                    <variable :find-prop="variable.find" :replace-prop="variable.replace"></variable>
+               
+            </template>
+            <br/>
+            <button class="btn btn-lg btn-primary" @click="addNewVariable()">Add new variable</button>
+    `,
+
+    data() {
+        return {
+            variableList: []
+        }
+    },
+
+    methods: {
+        addNewVariable() {
+            this.variableList.push({
+                find: "",
+                "replace": ""
+            })
+        }
+
+    },
+
+    created: function () {
+
+        let self = this
+        // get the list from api.
+        chrome.storage.sync.get("variableList", function (data) {
+            let list = data.variableList
+            if (!list) {
+                list = [{
+                    find: "{NAME}",
+                    replace: "Jhon"
+                }]
+            }
+            Vue.set(self, "variableList", list)
+        })
+    }
+
+})
+
+new Vue({el: '#variables'})
