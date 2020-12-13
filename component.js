65e5458372774a1d499fdf02667a30197fff08cd
naveen <curiousdonkey17@gmail.com>
2020-12-13 19:47:10 +0530

    Extracting templates; ;

--- component.js
+++ component.js
@@ -9,22 +9,7 @@ const bus = new Vue()
 
 Vue.component('variable', {
     props: ['variable', 'index'],
-    template: `<div class="card">
-  <div class="card-body bg-light">
-        <form class="form-inline">
-  <label class="sr-only" for="inlineFormInputName2">Variable</label>
-  <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="{NAME}" v-model="variable.find">
-    &nbsp; in the template will be replaced with &nbsp;
-  <label class="sr-only" for="inlineFormInputGroupUsername2">Value</label>
-  <div class="input-group mb-2 mr-sm-2">
-    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="ex: Jhon"  v-model="variable.replace">
-  </div>
-   <div class="input-group mb-2 mr-sm-2">
-    <button class="btn btn-danger btn-sm" @click="removeElement()" type="button">Delete</button>
-  </div>
-</form>
-  </div>
-</div>`,
+    template: '#variable_template',
     methods: {
         removeElement() {
             bus.$emit("remove-element", this.index )
@@ -35,17 +20,7 @@ Vue.component('variable', {
 
 Vue.component('variable-list', {
 
-    template: `
-
-        <template v-for="(variable, i) in variableList">
-            
-                    <variable :variable="variable" :index="i"></variable>
-               
-            </template>
-            <br/>
-            <button class="btn btn-sm btn-primary" @click="addNewVariable()">Add new variable</button>
-            <button class="btn btn-sm btn-success" @click="saveVariables()">Save</button>
-    `,
+    template: '#variable_list_template',
 
     data() {
         return {
